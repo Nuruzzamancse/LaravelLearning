16d73d180d552aa3f08f1bbb5f87df11b290b618
Nuruzzamancse <mr.nuruzzamancse@gmail.com>
2017-04-11 01:49:48 +0600

    bootstrap without internet

---
+++ Nothing/resources/views/file/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Document</title>
+	<link rel="stylesheet" type="text/css" href="/css/bootstrap.css">
+	<script src="/js/bootstrap.js" type="text/javascript"></script>
+</head>
+<body>
+
+<h1>Hello Bitfumes</h1>
+
+
+</body>
+</html>
